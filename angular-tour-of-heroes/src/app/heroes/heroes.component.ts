74c9346d2d03cf84fe7d87cfbe7e1da4be588dcf
Janify <107705505+JanifyPremium@users.noreply.github.com>
2024-04-08 16:02:16 +0200

    Update heroes component to display a list of heroes
    The heroes component template has been changed to display a list of heroes using \*ngFor directive.
    The heroes component class now includes an array of heroes and gets the data from a mock-heroes file.
    The component also imports NgFor, NgIf, UpperCasePipe and FormsModule from @angular/common package.
    The styles.css file includes a global style to change the background color of the main element to red.

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import {HEROES} from '../mock-heroes';
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
+  standalone: true, 
+  imports: [
+    NgFor,
+    NgIf,
+    UpperCasePipe,
+    FormsModule,
+  ],
 })
 export class HeroesComponent {
   hero: Hero = {
     id: 1,
     name: 'Viper'
   };
+  heroes = HEROES;
 }
